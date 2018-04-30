510871019f59aca04717d0a2baa04fbfff5e2fdd
Pranshul Saini <plsaini@ucdavis.edu>
2018-04-30 16:58:28 -0700

    It creates all of the triggers the way we want. I might make it a little more computationally effective but it is completely functional

--- update_triggers.m
+++ update_triggers.m
@@ -10,7 +10,6 @@ cond = zeros(num_sub,1);
 % 8 condition sequences
 conditions = importdata('D:\Stroop\Temp\conditions_sorted.xlsx');
 conditions = cell2mat(struct2cell(conditions));
-conditions(conditions == -1) = 2;  % 2 would be incongruent condition
 
 for i = 1:num_sub % 2nd file has incomplete event file. So a mismatch arises b/w log and event file
     i
@@ -18,8 +17,10 @@ for i = 1:num_sub % 2nd file has incomplete event file. So a mismatch arises b/w
     fid=fopen(filename);
     linenum = 7;      % This line number has condition written
     C = textscan(fid,'%s',1,'delimiter','\n', 'headerlines',linenum-1);
-    condition = C{1}{1}(29);
-    cond(i) = str2num(condition);
+    cond_num = C{1}{1}(29);  % finding condition number
+    cond(i) = str2num(cond_num);
+    
+    % finding 
     
     
     %link: https://www.mathworks.com/help/matlab/ref/textscan.html
@@ -39,7 +40,6 @@ for i = 1:num_sub % 2nd file has incomplete event file. So a mismatch arises b/w
     outcome = stim_len == resp_len;
     outcome = outcome + 399; % 400 for correct, 399 for incorrect
        
-    %deleting the messy reponses in the event file
     
     
     % finding the starting point for copying
@@ -48,13 +48,13 @@ for i = 1:num_sub % 2nd file has incomplete event file. So a mismatch arises b/w
     
     
     %defining the columns where data has to be copied from event files
-    n_rows_log = 2*length(stim); % two times because the event files has stimuli and responses separately listed
-    col1 = zeros(n_rows_log,1);
-    col2 = zeros(n_rows_log,1);
-    col3 = zeros(n_rows_log,1);
+    n_rows_log = length(stim); 
+    col1 = zeros(2*n_rows_log,1); % two times because the event files has stimuli and responses separately listed
+    col2 = zeros(2*n_rows_log,1);
+    col3 = zeros(2*n_rows_log,1);
     %col4 = char(zeros(n_rows_log,5));  
-    col4 = strings(n_rows_log,1);
-    col5 = zeros(n_rows_log,1);
+    col4 = strings(2*n_rows_log,1);
+    col5 = zeros(2*n_rows_log,1);
     
     %copy data to the columns 
     n_rows_evt = length(MyText{1,3}(:));
@@ -96,7 +96,7 @@ for i = 1:num_sub % 2nd file has incomplete event file. So a mismatch arises b/w
     old_triggers = col3;
     
     % updating triggers
-    cond_seq = conditions(:,cond(i));
+    cond_seq = conditions(:,4*cond(i)-3) + conditions(:,4*cond(i)-1) + 203; % the sum of 1st two terms lead to -2(incongruent), -1 (neutral in incongruent), 0 (neutral in neutral), 1 (neutral in congruent), 2 congruent 
     
     col3(1:2:end) = cond_seq;  % updating values for stimulus rows
     col3(2:2:end) = outcome;  % it accounts for none values as well
@@ -107,7 +107,7 @@ for i = 1:num_sub % 2nd file has incomplete event file. So a mismatch arises b/w
     
     
     % adding trial triggers
-    n_rows = (512*3)/2;
+    n_rows = n_rows_log*4;  % for every trial, we have 4 triggers: block number, trial number, condition, response
     
     col1_trial = int32(zeros(n_rows, 1));
     col2_trial = int32(zeros(n_rows, 1));
@@ -117,7 +117,7 @@ for i = 1:num_sub % 2nd file has incomplete event file. So a mismatch arises b/w
 
     %copying values to new column vectors
     for n = 1:256
-        j= 3*n -1;
+        j= 4*n -1;
         k = 2*n-1;
         col1_trial(j:j+1) = col1(k:k+1);
         col2_trial(j:j+1) = col2(k:k+1);
@@ -125,16 +125,30 @@ for i = 1:num_sub % 2nd file has incomplete event file. So a mismatch arises b/w
         col4_trial(j:j+1,:) = col4(k:k+1,:);
         col5_trial(j:j+1,:) = col5(k:k+1,:);
     end
-
-    %adding trial values
-    col1_trial(1:3:end) = col1(1:2:end) - 10;
+    
+    
+    %adding block number triggers
+    col1_trial(1:4:end) = col1(1:2:end);  % stimulus timestamp values
     col2_trial(:) = 1;
-    col3_trial(1:3:end,:) = 101:356;
+    col3_trial(1:4:end,:) = conditions(:,4*cond(i)-2);  % block number
     col4_trial =  string(col4_trial);
     col4_trial(:) = 'Trig.';
     col4_trial =  char(col4_trial);
     col5_trial = string(col5_trial);
-    col5_trial(1:3:end) = string(101:356);
+    col5_trial(1:4:end) = string(conditions(:,4*cond(i)-2));  % block number
+    col5_trial = char(col5_trial);
+    
+    
+    
+    %adding trial number triggers
+    col1_trial(2:4:end) = col1(1:2:end);  % stimulus timestamp values
+    col2_trial(:) = 1; 
+    col3_trial(2:4:end,:) = repmat(101:116,1,16)' ; 
+    col4_trial =  string(col4_trial);
+    col4_trial(:) = 'Trig.';
+    col4_trial =  char(col4_trial);
+    col5_trial = string(col5_trial);
+    col5_trial(2:4:end) = string(repmat(1:16,1,16)');
     col5_trial = char(col5_trial);
     
     
