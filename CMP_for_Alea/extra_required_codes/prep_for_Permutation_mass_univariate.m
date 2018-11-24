b0da5de0735a040d3acdbdb02b67a1d7902252e6
pranshulsaini <pranshulsaini17@gmail.com>
2018-11-24 17:19:14 +0530

    Delete CMP_for_Alea/extra_required_codes/prep_for_Permutation_mass_univariate.m

--- CMP_for_Alea/extra_required_codes/prep_for_Permutation_mass_univariate.m
+++
@@ -1,9 +0,0 @@
-trl=EEG.trials;
-code='31_444_2';
-for i=1:trl
-EEG.epoch(i).eventtype(1)={code};
-EEG.event(2*i-1).type=code;
-EEG.urevent(i).type=code;
-end
-[ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG,1);
-EEG = eeg_checkset(EEG, 'eventconsistency');
